ab639cc90cf65cb518f085cdfe5691c2c8afe688
atdhamov <atxamovasilbek3@gmail.com>
2025-09-27 17:56:53 +0500

    Add files via upload

---
+++ js.js
@@ -0,0 +1,7 @@
+let ism = prompt(`ismingizni kritnig`)
+let familya = prompt(`familyangizni kriting`)
+let yosh = prompt(`yoshingizni kriting`)
+let shahar = prompt(`qaysi shaharda turishingizni kirting`)
+let viloyet = prompt(`qaysi viloyatdan ekanligingizni kirting`)
+
+alert(`salom, mening ismim ${ism} , familya ${familya}, yoshim ${yosh}, shahar ${shahar},`)
